cbdc2c8dc6b17d45c2d7fb22156fbb1973c41139
Charles Cao <i@caoyue.me>
2014-11-26 17:15:48 +0800

    push to multiple resources after privatemode set

--- apps/ejabberd/src/mod_privatemode.erl
+++ apps/ejabberd/src/mod_privatemode.erl
@@ -24,6 +24,7 @@ set_contact(From, _To, #iq{sub_el = SubEl} = IQ) ->
     Private = xml:get_tag_attr_s(<<"private">>, SubEl),
     case odbc_privatemode:set_contact(LServer, LUser, ContactJid, Private) of
         ok ->
+            multiple_resources_broadcast(LUser, LServer, ContactJid, Private, <<"contact">>),
             IQ#iq{type = result, sub_el = [SubEl]};
         _ ->
             IQ#iq{type = error, sub_el = [SubEl]}
@@ -38,10 +39,25 @@ set_group(From, _To, #iq{sub_el = SubEl} = IQ) ->
     Private = xml:get_tag_attr_s(<<"private">>, SubEl),
     case odbc_privatemode:set_group(LServer, UserJid, GroupId, Private) of
         ok ->
+            multiple_resources_broadcast(LUser, LServer, GroupId, Private, <<"group">>),
             IQ#iq{type = result, sub_el = [SubEl]};
         _ ->
             IQ#iq{type = error, sub_el = [SubEl]}
     end.
+
+multiple_resources_broadcast(LUser, LServer, ContactJid, Private, Mode) ->
+    FromJid = jlib:make_jid(<<>>, <<>>, <<>>),
+    ToJid = jlib:make_jid(LUser, LServer, <<>>),
+    LangAttr = {<<"xml:lang">>, <<"en">>},
+    ToAttr = {<<"to">>, jlib:jid_to_binary(ToJid)},
+    TypeAttr = {<<"type">>, <<"chat">>},
+    Packet = {xmlel, <<"message">>, [],
+        [{xmlel, <<"push">>, [{<<"xmlns">>, ?NS_PRIVATEMODE}, {<<"type">>, <<"privatemode">>},
+            {<<"mode">>, Mode},{<<"subject">>, ContactJid}, {<<"private">>, Private}],
+            [{xmlcdata, <<>>}]}
+        ]},
+    ejabberd_router:route(FromJid, ToJid,
+        Packet#xmlel{attrs = [ToAttr, TypeAttr, LangAttr]}).
 
 is_query(Packet) ->
     case Packet of
